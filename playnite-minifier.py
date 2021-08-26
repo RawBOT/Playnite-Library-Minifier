11c44f40bda25d1b2ec630c78e468971e1a83060
RawBOT <rodwilhelmy@gmail.com>
2021-08-26 16:55:40 +0200

    Convert PNGs into JPGs

--- playnite-minifier.py
+++ playnite-minifier.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
         # Get all JPG files inside Playnite's library path
         # PNG files are usually used on smaller images, so are ignored
         image_files = glob.glob(glob.os.path.join(library_path, '**', '*.jpg'), recursive=True)
+        image_files.extend(glob.glob(glob.os.path.join(library_path, '**', '*.png'), recursive=True))
         for file in image_files:
             if VERBOSE: print("PROCESSING: {0}".format(file))
             if glob.os.path.getsize(file) == 0:
@@ -21,7 +22,14 @@ if __name__ == "__main__":
                 continue
             try:
                 with Image.open(file) as im:
-                    im.save(file, "JPEG", quality=JPG_QUALITY)
+                    if im.format == "JPEG":
+                        im.save(file, "JPEG", quality=JPG_QUALITY)
+                    elif im.format == "PNG":
+                        new_filename = glob.os.path.splitext(file)[0] + '.jpg'
+                        im.save(new_filename, "JPEG", quality=JPG_QUALITY)
+                        glob.os.remove(file)
+                    else:
+                        if VERBOSE: print("ERROR: Invalid image format {0} for file: {1}".format(im.format, file))
             except OSError:
                 print("Cannot process: {0}".format(file))
     else:
